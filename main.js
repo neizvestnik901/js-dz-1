5806f86c3ee525b247007422d0e7012e330d01ff
jurabek <qwert@gmail.com>
2024-08-18 12:06:43 +0500

    origin

---
+++ main.js
@@ -0,0 +1,44 @@
+ 
+var text = prompt('Ведите имя') 
+
+console.log('Меня зовут ' + text)
+
+
+var age = prompt('Сколько вам лет?')
+
+console.log('Мне ' + age + ' лет')
+  
+var example = prompt('Сколько будет 6 - 3 = ?')
+
+console.log( "6 - 3 = 3 " + ' Ваш ответ ' + example ) 
+
+var example = prompt('Сколько будет 20 - 5 = ?')
+
+console.log( "20 - 5 = 15 " + ' Ваш ответ ' + example )
+
+var example = prompt('Сколько будет 15 * 2  = ?')
+
+console.log( "15 * 2 = 30 " + ' Ваш ответ ' + example )
+
+var example = prompt('Сколько будет 8 / 4  = ?')
+
+console.log( "8 / 4 = 2 " + ' Ваш ответ ' + example )
+
+var example = prompt('Сколько будет 10 % 3  = ?')
+
+console.log( "10 % 3 = 1 " + ' Ваш ответ ' + example )
+
+var example = alert('Зайдите в консоль')  
+
+//Задача 2
+
+let X = Number(prompt('Введите первое число'));
+
+let Y = Number(prompt('Введите второе число'));
+
+let Z = Number(prompt('Введите третье число'));
+
+res = (X + Y + Z) / 3;
+ 
+console.log("Среднее арифметическое: " + res);
+
